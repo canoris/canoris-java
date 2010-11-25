89cfd307eb6666a5158720c723b25c0b81822c29
stelios <stetogias@gmail.com>
2010-11-25 17:32:25 +0100

    Added javadoc

--- src/com/canoris/exception/CanorisException.java
+++ src/com/canoris/exception/CanorisException.java
@@ -1,11 +1,26 @@
 package com.canoris.exception;
 
-import java.io.IOException;
-
+/**
+ * CanorisException is thrown when a JsonParseException occurs.
+ * We wrap the JsonParseException and extract the HTTP error code
+ * from the HttpResponse object. Essentially this is the only 
+ * meaningful part of the response in case of an error. 
+ * 
+ * @author stelios
+ *
+ */
 public class CanorisException extends Exception {
 	
 	private int httpErrorCode;
 
+	/**
+	 * Parameterized constructor.
+	 * 
+	 * @param code
+	 * 			The HTTP error code
+	 * @param cause
+	 * 			The initial exception (Throwable)
+	 */
 	public CanorisException(int code, Throwable cause) {
 		super(String.valueOf(code), cause);
 		this.httpErrorCode = code;
