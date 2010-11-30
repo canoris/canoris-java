c82c16da1076ee5c570a04c0ce56a3e633a27c15
stelios <stetogias@gmail.com>
2010-11-30 15:58:50 +0100

    Mavenized project - NO code changes

---
+++ java-client/src/main/java/com/canoris/api/CanorisAPI.java
@@ -0,0 +1,46 @@
+package com.canoris.api;
+
+/**
+ * CanorisAPI class is used to simply store the api_key 
+ * and baseURL which are used in all requests.
+ * 
+ * @author stelios
+ */
+public class CanorisAPI {
+	
+	// base URL
+	// Can be set for testing purposes
+	private String baseURL = "api.canoris.com";
+	
+	// The api_key used in all requests
+	private String apiKey = "";
+	
+	private static CanorisAPI instance = null;
+		protected CanorisAPI() {
+	}
+		
+	public static CanorisAPI getInstance() {
+		if(instance == null) {
+			instance = new CanorisAPI();
+	    }
+	    return instance;
+	}
+	
+	/* ************* GETTERS && SETTERS ************** */
+	public String getApiKey() {
+		return apiKey;
+	}
+
+	public String getBaseURL() {
+		return baseURL;
+	}
+
+	public void setBaseURL(String baseURL) {
+		this.baseURL = baseURL;
+	}
+
+	public void setApiKey(String apiKey) {
+		this.apiKey = apiKey;
+	}
+	
+}
---
+++ java-client/src/main/java/com/canoris/api/Constants.java
@@ -0,0 +1,36 @@
+package com.canoris.api;
+
+/**
+ * Constants class. It actually holds all the resource paths 
+ * used in the requests.
+ * 
+ * @author stelios
+ *
+ */
+public class Constants {
+	public final static String URI_FILES = "/files";
+	public final static String URI_FILE = "/files/$fileKey$";
+	
+	public final static String URI_FILE_SERVE = "/files/$fileKey$/serve";
+	public final static String URI_FILE_ANALYSIS = "/files/$fileKey$/analysis/$filter$";
+	
+	public final static String URI_FILE_CONVERSIONS = "/files/$fileKey$/conversions";
+	public final static String URI_FILE_CONVERSION = "/files/$fileKey$/conversions/$conversion$";
+	
+	public final static String URI_FILE_VISUALIZATIONS = "/files/$fileKey$/visualizations";
+	public final static String URI_FILE_VISUALIZATION = "/files/$fileKey$/visualizations/$visualization$";
+
+	public final static String URI_COLLECTIONS = "/collections";
+	public final static String URI_COLLECTION = "/collections/$collectionKey$";
+	public final static String URI_COLLECTION_FILES = "/collections/$collectionKey$/files";
+	public final static String URI_COLLECTION_FILE = "/collections/$collectionKey$/files/$fileKey$";
+	public final static String URI_COLLECTION_SIMILAR = "/collections/$collectionKey$/similar/$fileKey$/$preset$/$results$";
+	
+	public final static String URI_TEMPLATES = "/processing/templates";
+	public final static String URI_TEMPLATE = "/processing/templates/$templateName$";
+	
+	public final static String URI_TASKS = "/processing/tasks";
+	public final static String URI_TASK = "/processing/tasks/$taskId$";
+	
+	public final static String URI_PHONEMES = "/language/text2phonemes";
+}
